93ff1709c19ad59e85615f7f08a144e16f702d71
rthway <rthway@gmail.com>
2023-12-29 11:29:52 +0545

    add header_ads with code and img options

--- functions.php
+++ functions.php
@@ -7,3 +7,40 @@ function roshanpress_custom_logo_setup() {
 }
 add_action('after_setup_theme', 'roshanpress_custom_logo_setup');
 
+
+// header_ads
+function custom_theme_customizer($wp_customize) {
+    // Add a section for header options
+    $wp_customize->add_section('header_section', array(
+        'title' => __('Header Options', 'custom-theme'),
+        'priority' => 30,
+    ));
+
+    // Add a setting for header_ads_image (image upload)
+    $wp_customize->add_setting('header_ads_image', array(
+        'default' => '',
+        'transport' => 'refresh',
+    ));
+
+    // Add a control for header_ads_image
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'header_ads_image', array(
+        'label' => __('Header Advertisement Image (Upload)', 'custom-theme'),
+        'section' => 'header_section',
+        'settings' => 'header_ads_image',
+    )));
+
+    // Add a setting for header_ads_code (text area for code)
+    $wp_customize->add_setting('header_ads_code', array(
+        'default' => '',
+        'transport' => 'refresh',
+    ));
+
+    // Add a control for header_ads_code
+    $wp_customize->add_control('header_ads_code', array(
+        'label' => __('Header Advertisement Code', 'custom-theme'),
+        'section' => 'header_section',
+        'type' => 'textarea',
+    ));
+}
+
+add_action('customize_register', 'custom_theme_customizer');
--- header.php
+++ header.php
@@ -20,8 +20,18 @@
         <?php } ?>
     </div>
 
-    <div class="header-ad">
-        <?php do_action('roshanpress_header_ad'); // Display the header ad ?>
+    <div class="header-ads">
+        <?php
+        // Display header_ads_image if available, otherwise display header_ads_code
+        $header_ads_image = get_theme_mod('header_ads_image', '');
+        $header_ads_code = get_theme_mod('header_ads_code', '');
+
+        if (!empty($header_ads_image)) {
+            echo '<img src="' . esc_url($header_ads_image) . '" alt="Header Advertisement Image">';
+        } elseif (!empty($header_ads_code)) {
+            echo $header_ads_code;
+        }
+        ?>
     </div>
 </header>
 
